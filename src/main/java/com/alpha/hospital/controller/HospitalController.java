856387269d79e141f85beb6ba56dfda29f81e198
Arun Bhukya <arunbhukyaa1@gmail.com>
2025-11-27 17:45:08 +0530

    controller

--- src/main/java/com/alpha/hospital/controller/HospitalController.java
+++ src/main/java/com/alpha/hospital/controller/HospitalController.java
@@ -1,5 +1,42 @@
 package com.alpha.hospital.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.alpha.hospital.entity.Doctor;
+import com.alpha.hospital.services.HospitalService;
+
 public class HospitalController {
+	@Autowired
+	private HospitalService hs;
+	
+	@PostMapping("/saveDoctor")
+	public void saveDoctor(@RequestBody Doctor d) {
+		hs.saveDoctor(d);
+	}
+	
+	
+	@GetMapping("/findDcotor")
+	public ResponseStructure<Doctor> findDoctor(@RequestParam int did) {
+		return hs.findDoctor(did);
+	}
+	
+	
+	@DeleteMapping("/deleteStudent")
+	public void deleteDoctor(@RequestParam int did) {
+		hs.deleteDoctor(did);
+	}
+	
+	
+	@PutMapping("/updateDoctor")
+	public void updateDoctor(@RequestParam int did,@RequestParam String dname,@RequestParam String specilization) {
+		hs.updateDoctor(did,dname,specilization);
+	}
+
 
 }
--- src/main/java/com/alpha/hospital/services/HospitalService.java
+++ src/main/java/com/alpha/hospital/services/HospitalService.java
@@ -1,5 +1,28 @@
 package com.alpha.hospital.services;
 
+import com.alpha.hospital.controller.ResponseStructure;
+import com.alpha.hospital.entity.Doctor;
+
 public class HospitalService {
+
+	public void saveDoctor(Doctor d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteDoctor(int did) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateDoctor(int did, String dname, String specilization) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ResponseStructure<Doctor> findDoctor(int did) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
