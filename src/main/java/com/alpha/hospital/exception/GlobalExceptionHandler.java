72e705875553e6697234ba9cf840dc20dcde76ee
Jeevan Biradar <jeevanbiradar668@gmail.com>
2025-11-28 14:02:05 +0530

    added globalexception handler methods

--- src/main/java/com/alpha/hospital/exception/GlobalExceptionHandler.java
+++ src/main/java/com/alpha/hospital/exception/GlobalExceptionHandler.java
@@ -1,7 +1,15 @@
 package com.alpha.hospital.exception;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,5 +30,46 @@ public class GlobalExceptionHandler {
 
 	        return new ResponseEntity<>(rs, HttpStatus.NOT_FOUND);
 	    }
+	 
+	 
+	 
+	 @ExceptionHandler(DataIntegrityViolationException.class)
+	   public ResponseStructure<String> handleDataIntegrity(DataIntegrityViolationException ex)
+	   {
+		   ResponseStructure<String> rs = new ResponseStructure<>();
+		    rs.setStatusCode(HttpStatus.CONFLICT.value());
+		    rs.setMessage("Duplicate ID or foreign key violation");
+		    rs.setData(ex.getMostSpecificCause().getMessage());
+		    return rs;
+	   }
+	 
+	 
+	 @ExceptionHandler(MethodArgumentNotValidException.class)
+	 public ResponseStructure<Map<String, String>> handleMethodArgNotValid(MethodArgumentNotValidException ex)
+	 {
+		 Map<String, String> errormap = new HashMap<String, String>();
+		 
+		 List<ObjectError> objerror = ex.getAllErrors();
+		 
+		 for(ObjectError objectError : objerror)
+		 {
+			 System.out.println(objectError.getDefaultMessage());
+			 
+			 FieldError fr = (FieldError) objectError;
+			 System.err.println(fr.getField());
+			 
+			 errormap.put(fr.getField(), objectError.getDefaultMessage());
+		 }
+		 
+		 ResponseStructure<Map<String,String>> rs = new ResponseStructure<Map<String,String>>();
+		 
+		 rs.setStatusCode(HttpStatus.NOT_ACCEPTABLE.value());
+		 rs.setMessage("validation Failed");
+		 rs.setData(errormap);
+		 
+		 return rs;
+	 }
 
+	 
+	  
 }
